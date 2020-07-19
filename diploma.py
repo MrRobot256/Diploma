fb46517b92424bba392cf7766a9cc5e359140467
ivansviridov <mr.root256@gmail.com>
2020-07-19 15:50:26 +0300

    add requirements and other things

--- diploma.py
+++ diploma.py
@@ -2,10 +2,11 @@ import json
 import time
 from User import User
 
-USER_ID = input('Введите ID пользователя: ')
-
 
 def uncommon_group(user_id):
+    """
+    функция запускает сканирование пользователей и проверяет их по группам
+    """
     main_user = User(user_id)
     time.sleep(1/3)
     friends_set = main_user.get_friends()
@@ -31,6 +32,9 @@ def uncommon_group(user_id):
 
 
 def get_data():
+    """
+    функция формирует список групп и параметры, а именно id группы, название группы, кол-во участников в группе
+    """
     data = []
     user = User(USER_ID)
     unique_groups = uncommon_group(USER_ID)
@@ -45,8 +49,14 @@ def get_data():
 
 
 def dump_json():
+    """
+    функция записывает в .json файл запрашиваемые свойства из функции  get_data
+    """
     with open('groups.json', 'w', encoding='utf-8') as fo:
-        json.dump(get_data(), fo, ensure_ascii=False, indent=2)
+        json.dump(DATA, fo, ensure_ascii=False, indent=2)
 
 
-dump_json()
+if __name__ == '__main__':
+    USER_ID = input('Введите ID пользователя: ')
+    DATA = get_data()
+    dump_json()
